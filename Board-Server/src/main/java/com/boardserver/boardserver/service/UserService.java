f0f65d61e7537597f1332c3e8d8c04e6ca9cc48e
sample01 <minihama@hotmail.co.kr>
2024-10-22 08:17:00 +0900

    change UserService class -> interface

--- Board-Server/src/main/java/com/boardserver/boardserver/service/UserService.java
+++ Board-Server/src/main/java/com/boardserver/boardserver/service/UserService.java
@@ -2,7 +2,10 @@ package com.boardserver.boardserver.service;
 
 import com.boardserver.boardserver.dto.UserDTO;
 
-public class UserService {
+public interface UserService {
+
+    
+} UserService {
     // userservice 는 메서드의 시그니처만 작성하고 메서드 내용은 작성 안함
 
     // user의 회원가입
@@ -14,7 +17,7 @@ public class UserService {
     // 정보
     UserDTO getUserInfo(String userId);
     // 패스워드 변경
-    void updatePassword(String id, Sting beforePassword, String afterPassword);
+    void updatePassword(String id, String beforePassword, String afterPassword);
     // 삭제
     void deleteId(String id, String password);
 }
