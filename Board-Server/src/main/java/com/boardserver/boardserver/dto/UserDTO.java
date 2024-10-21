2b8909bdb617f363b37da293aed36e7c380ac69c
sample01 <minihama@hotmail.co.kr>
2024-10-22 08:24:11 +0900

    import userdto date

--- Board-Server/src/main/java/com/boardserver/boardserver/dto/UserDTO.java
+++ Board-Server/src/main/java/com/boardserver/boardserver/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.boardserver.boardserver.dto;
 
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,7 +19,7 @@ public class UserDTO {
     public enum Status{
         DEFAULT, ADMIN, DELETED
     }
-
+    // private 를 사용한 캡슐화
     // ERD 에 작성 한 테이블 기반으로 생성
     private int id;
     private String userId;
